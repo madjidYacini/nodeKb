be0fcf63cc8055c6819dfdc7a0afab6ee4cec13e
madjid yacini <madjid.yacini@gmail.com>
2018-05-13 14:29:51 +0200

    adding messages

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require ("mongoose");
- const bodyParser= require("body-parser"); 
+const bodyParser= require("body-parser"); 
+const session = require("express-session");
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+
 // connction to mongodb
+
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
 
@@ -21,7 +26,9 @@ const app = express();
 // bring in model 
 const Article = require('./models/article')
 
-
+//calling article route 
+// const articles = require('./routes/articles');
+// app.use('/articles',articles)
 // body parser middleWare
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,6 +42,40 @@ app.use(express.static(path.join(__dirname,'public')));
 // load view engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
+
+//express session middleWare 
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+   
+  })
+);
+// express messages 
+app.use(require("connect-flash")());
+app.use(function(req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
+
+// express validator midlleware
+app.use(expressValidator({
+    errorFormatter: function (param,msg,value) {
+        var namespace =param.split('.')
+        ,root = namespace.shift()
+        ,formParam = root;
+        while (namespace.length) {
+            formParam+='['+namespace.shift()+']';
+        }
+        return{
+            param : formParam,
+            msg : msg ,
+            value :value
+        }
+    }
+}))
 // home route
 app.get('/',(req,res)=>{
  Article.find({},(err, articles)=>{
@@ -54,24 +95,36 @@ app.get('/articles/add',(req,res)=>{
 })
 // add submit post Route
 app.post('/articles/add',(req,res)=>{
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
+ req.checkBody("title", "the title is required").notEmpty();
+ req.checkBody("author", "the author is required").notEmpty();
+ req.checkBody("body", "the body is required").notEmpty();
 
-    article.save((err)=>{
-       if(err){
-        console.log(err);
-        return;
-       }else{
-        res.redirect('/')
-       }
-    })
+ let errors = req.validationErrors();
+ console.log("toooooz", req.body);
+
+ if (errors) {
+   res.render("add_article", { title: "add articles", errors: errors });
+ } else {
+   let article = new Article();
+   article.title = req.body.title;
+   article.author = req.body.author;
+   article.body = req.body.body;
+
+   article.save(err => {
+     if (err) {
+       console.log(err);
+       return;
+     } else {
+       req.flash("success", "article added");
+       res.redirect("/");
+     }
+   });
+ }
     // console.log(req.body.title );
 })
 
 // get single article
-app.get('/article/:id',(req,res)=>{
+app.get('/articles/:id',(req,res)=>{
     Article.findById(req.params.id,(err,article)=>{
       res.render("article",{
           article:article
@@ -79,7 +132,7 @@ app.get('/article/:id',(req,res)=>{
     })
 })
 // load edit form
-app.get('/article/edit/:id',(req,res)=>{
+app.get('/articles/edit/:id',(req,res)=>{
     Article.findById(req.params.id,(err,article)=>{
       res.render("edit_article",{
           title:"Edit article",
@@ -100,6 +153,7 @@ app.post('/articles/edit/:id',(req,res)=>{
         console.log(err);
         return;
        }else{
+       req.flash("success", "article updated");
         res.redirect('/')
        }
     })
@@ -122,3 +176,4 @@ app.delete('/article/:id',(req, res)=>{
 app.listen(3000,()=>{
     console.log("listing on 3000");
 })
+// start server
